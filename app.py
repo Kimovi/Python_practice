142a80202af3c37d998501f615a878ef51118f60
Bora Kim <stellakkkk@hotmail.com>
2021-01-31 19:28:17 +0000

    Flask and SQLAlchemy excercise

--- app.py
+++ app.py
@@ -5,40 +5,48 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/Register"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/Register"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/Games" #"sqlite:///data.db"
-
 
 db = SQLAlchemy(app)
 
-class Register(db.Model):
-    name = db.Column(db.String(30), nullable=False, primary_key=True)
+# class Register(db.Model):
+#     name = db.Column(db.String(30), primary_key=True)
+
+class Book(db.Model):
+    book_title = db.Column(db.String(30), primary_key=True, autoincrement=True)
+    # book_id = db.Column(db.Integer, db.ForeignKey("register.name"), nullable=False)
+    # date_finished = db.Column(db.DateTime)
+    # rating = db.Column(db.Integer)
+    # review = db.Column(db.String(30))
 
 db.create_all()
 
 @app.route('/', methods=["GET","POST"])
 def home():
     if request.form:
-        person = Register(name=request.form.get("name"))
-        db.session.add(person)
+        # person = Register(name=request.form.get("name"))
+        book = Book(book_title=request.form.get("book_title"))
+        db.session.add(book)
         db.session.commit()
-    registrees = Register.query.all()
-    return render_template("home.html", registrees=registrees)
+    # registrees = Register.query.all() 
+    book_lists = Book.query.all()
+    return render_template("home.html", book_lists=book_lists)
 
-@app.route("/update", methods=["POST"])
-def update():
-    person = Register.query.filter_by(name=request.form.get("oldname")).first()
-    person.name = request.form.get("newname")
-    db.session.commit()
-    return redirect("/")
+# @app.route("/update", methods=["POST"])
+# def update():
+#     person = Register.query.filter_by(name=request.form.get("oldname")).first()
+#     person.name = request.form.get("newname")
+#     db.session.commit()
+#     return redirect("/")
 
-@app.route("/delete", methods=["POST"])
-def delete():
-    person = Register.query.filter_by(name=request.form.get("name")).first()
-    db.session.delete(person)
-    db.session.commit()
-    return redirect("/")
+# @app.route("/delete", methods=["POST"])
+# def delete():
+#     person = Register.query.filter_by(name=request.form.get("name")).first()
+#     db.session.delete(person)
+#     db.session.commit()
+#     return redirect("/")
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
